aee32293f0e2fab03e1c8e05f4300a9d1304e712
Timothy Dunk <tim@dunk.id.au>
2020-12-11 11:04:32 +1100

    added pseudocode in index.rb

---
+++ src/index.rb
@@ -0,0 +1,27 @@
+# Welcome message
+# Explain rules of the game to user
+# Press Enter to Continue, "i" for more information, or "e" to Exit
+
+#if input == i or I
+# puts more information about rules of the game
+# elsif input == e or E
+# puts "Have a good day!"
+# exits
+# else
+# continue
+
+# Select a time limit
+# Select a prompt category
+# Random prompt in category selected
+# Prompt is shown to user
+# Timer starts
+# gets from user pushes into array
+
+# When timer finishes if array size == 0
+# puts "Sorry! You got nothing"
+# if array size == 1 
+# puts "Congratulations! You came up with 1 idea! You gotta start somewhere."
+# puts idea from array
+# else
+# puts "Congratulations! You came up with number ideas!"
+# puts numbered list of ideas from array
