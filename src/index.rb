8190d97444cb1bdff346c02e45210a48859b0993
Timothy Dunk <tim@dunk.id.au>
2020-12-17 09:45:31 +1100

    fixed inconsistent single/double quote usage

--- src/index.rb
+++ src/index.rb
@@ -1,4 +1,4 @@
-require 'timers'
+require "timers"
 
 # fix gets
 def gets
@@ -8,24 +8,24 @@ end
 # Welcome message
 def welcome_menu()
     # clear previous text on terminal
-    system('clear')
+    system("clear")
     # welcome text
-    puts 'Welcome to the Idea Game!', ''
-    puts 'The aim of this game is to come up with as many ideas as possible' 
-    puts 'within the time limit.', ''
-    puts 'You will be given a prompt to inspire the ideas,' 
-    puts 'and be able to choose a time limit.', ''
+    puts "Welcome to the Idea Game!", ""
+    puts "The aim of this game is to come up with as many ideas as possible" 
+    puts "within the time limit.", ""
+    puts "You will be given a prompt to inspire the ideas," 
+    puts "and be able to choose a time limit.", ""
     puts "Press 1 to Continue."
     puts "Press 2 to see about information."
     puts "Press 3 to Exit."
     menu_input = gets.chomp
     # exit
     if menu_input.to_i == 3
-        puts 'Exiting...', 'Have a good day!'
+        puts "Exiting...", "Have a good day!"
     # about
     elsif menu_input.to_i == 2
-        puts 'This game was created by Tim Dunk in 2020.'
-        puts 'press enter to continue'
+        puts "This game was created by Tim Dunk in 2020."
+        puts "Press enter to continue."
         gets
         welcome_menu()
     # continue
@@ -33,59 +33,60 @@ def welcome_menu()
         play_game()
     # saving invalid inputs
     else
-        puts 'Invalid input. Please try again. Press enter to continue.'
+        puts "Invalid input. Please try again." 
+        puts "Press enter to continue."
         gets
         welcome_menu()
     end
 end
 
 def play_game()
-    puts '', 'Select a timer amount:'
-    puts 'Press 1 for 30 seconds.'
-    puts 'Press 2 for 1 minute.'
-    puts 'Press 3 for 2 minutes.'
-    puts 'Press 4 for 5 minutes.'
+    puts "", "Select a timer amount:"
+    puts "Press 1 for 30 seconds."
+    puts "Press 2 for 1 minute."
+    puts "Press 3 for 2 minutes."
+    puts "Press 4 for 5 minutes."
     time_input = gets.chomp.to_i
     case time_input
     when 1
-        puts 'You have selected 30 seconds.'
+        puts "You have selected 30 seconds."
         time_select = 30
     when 2
-        puts 'You have selected 1 minute.'
+        puts "You have selected 1 minute."
         time_select = 60
     when 3
-        puts 'You have selected 2 minutes.'
+        puts "You have selected 2 minutes."
         time_select = 120
     when 4
-        puts 'You have selected 5 minutes.'
+        puts "You have selected 5 minutes."
         time_select = 300
     else 
         puts "Invalid input. Please try again."
         play_game()
     end
-    puts '', 'Select a prompt category:'
-    puts 'Press 1 for band names.'
-    puts 'Press 2 for sketch ideas.'
-    puts 'Press 3 for observations.'
-    puts 'Press 4 for business ideas.'
+    puts "", "Select a prompt category:"
+    puts "Press 1 for band names."
+    puts "Press 2 for sketch ideas."
+    puts "Press 3 for observations."
+    puts "Press 4 for business ideas."
     prompt_num = Random.rand(5)
     prompt_input = gets.chomp.to_i
     case prompt_input
     when 1
         band_names = ["Snail", "Mouse", "Cold", "Valley", "Steep"]
-        puts 'You have selected band names.'
+        puts "You have selected band names."
         prompt = "Come up with as many band names that contain the word #{band_names[prompt_num]} as you can."
     when 2
         sketch_ideas = ["Restaurant", "Soup", "Doctor", "Grapes", "Houseplant"]
-        puts 'You have selected sketch ideas.'
+        puts "You have selected sketch ideas."
         prompt = "Come up with as many sketch ideas as you can, inspired by the word #{sketch_ideas[prompt_num]}."
     when 3
         observations = ["Tea", "Breakfast", "Headlice", "Glass", "An Orange"]
-        puts 'You have selected observations.'
+        puts "You have selected observations."
         prompt = "Write as many observations about #{observations[prompt_num]} as you can."
     when 4
         business_ideas = ["Salt", "Bicycles", "Dating", "Technology", "Books"]
-        puts 'You have selected business ideas.'
+        puts "You have selected business ideas."
         prompt = "Come up with as many business ideas as you can, inspired by the word #{business_ideas[prompt_num]}"
     else 
         puts "Invalid input. Please try again."
@@ -99,9 +100,9 @@ def guessing(number, phrase)
     timers = Timers::Group.new
     still_time = true
     puts phrase
-    puts 'Go!'
+    puts "Go!"
     thirty_second_timer = timers.after(number) {still_time = false}
-    thirty_second_timer2 = timers.after(number) {puts '', 'Time\'s up! Press enter to continue.'}
+    thirty_second_timer2 = timers.after(number) {puts "", "Time's up! Press enter to continue."}
     timer_thread = Thread.new {timers.wait}
     for num in 1..100 do
         if still_time
